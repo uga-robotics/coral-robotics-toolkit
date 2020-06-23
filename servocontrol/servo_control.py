a4132c7d7292428fbe0d1f8b0eecbdc5be00c9be
Jyumpp <hdhdog@live.com>
2020-06-23 16:42:48 -0400

    comment

--- servocontrol/servo_control.py
+++ servocontrol/servo_control.py
@@ -24,7 +24,7 @@ class Servo:
 
     def loop(self):
         while self.looping:
-            self.debug.info(str(self.looping))
+            # self.debug.info(str(self.looping))
             self.pin.write(True)
             time.sleep(self.millis / 1000)
             self.pin.write(False)
